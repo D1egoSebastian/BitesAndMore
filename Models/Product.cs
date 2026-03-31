5bfd4fb58ec25518e7dfbbaa44ee21fd376db917
D1egoSebastian <sebastiandiegorr@gmail.com>
2026-03-31 19:40:36 -0400

    product and category models

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace BitesAndMore.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int? Price { get; set; }
+        public string? imageUrl { get; set; }
+        public int CategoryId { get; set; }
+        //category model
+        public Category? Category { get; set; }
+        public bool isAvailable { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
