3c9ccacd04a848fb3969ca7fa115bfb1233da961
D1egoSebastian <sebastiandiegorr@gmail.com>
2026-04-19 23:15:04 -0400

    fix authorization line

--- Program.cs
+++ Program.cs
@@ -59,7 +59,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
-app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.UseCors("AllowAll");
 
